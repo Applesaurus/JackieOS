30f97dd884554e358a9c892214f11c8fd35323ef
Jackie Bowers <hennervate@tutanota.com>
2019-01-16 05:09:34 +0000

    Update bootsector-test.asm

--- bootsector-test.asm
+++ bootsector-test.asm
@@ -1,26 +1,27 @@
-mov ah, 0x0e ; tty mode
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10 
-mov al, 'o'
-int 0x10
-mov al, ' '
-int 0x10
-mov al, 'W'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, 'r'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'd'
+[BITS 16]
+[ORG 0]
 
+jmp 0x7C0:main   ; Goto segment 07C0
+
+main:
+    mov si, msg
+    call print
+    
 jmp $ ; jump to current address
+    
+print:
+    lodsb
+    or al, 0 ; if al is 0, done
+    jz done
+    mov ah, 0x0e ; tty mode
+    mov bh, 0;
+    int 0x10; print al w/ tty mode
+    jmp bios_print
+    
+done:
+    ret
+
+msg   db 'Hello World', 13, 10, 0
 
 ; padding and magic number
 times 510 - ($-$$) db 0
