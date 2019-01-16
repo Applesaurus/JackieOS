0704652148ac29daac2257ba085d11ce23825f89
Jackie Bowers <hennervate@tutanota.com>
2019-01-16 05:27:31 +0000

    Update bootsector-test.asm

--- bootsector-test.asm
+++ bootsector-test.asm
@@ -1,22 +1,34 @@
 [BITS 16]
 [ORG 0]
 
-jmp 0x7C0:main   ; Goto segment 07C0
+jmp 0x7C0:main   ; goto segment 07C0
 
 main:
+    xor ax, ax
+    mov ds, ax
+    mov ss, ax ; set stack segment to 0 and stack pointer to well past code to be executed
+    mov sp, 0x9c00
+    cld
+    
     mov si, msg
-    call print
+    call print-bios
+    
+    
     
 jmp $ ; jump to current address
     
-print:
+print-bios:
     lodsb
-    or al, 0 ; if al is 0, done
-    jz done
+    test al, al ; if al is 0, done
+    jz done; 
     mov ah, 0x0e ; tty mode
     mov bh, 0;
     int 0x10; print al w/ tty mode
     jmp bios_print
+    
+printc:
+
+prints:
     
 done:
     ret
