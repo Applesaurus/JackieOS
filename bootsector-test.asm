40a233a8e61ec67a9173597733640af2450ebf98
Jackie Bowers <hennervate@tutanota.com>
2019-01-17 02:36:30 +0000

    Update bootsector-test.asm

--- bootsector-test.asm
+++ bootsector-test.asm
@@ -13,6 +13,14 @@ main:
     mov si, msg
     call print-bios
     
+    call prints
+    mov gs, 0xb800 ; video memory
+    mov bx, 0x0000 ; video memory offset
+    mov ax, [gs:bx]
+    
+    mov word [reg16], ax
+    call print16
+    
     
     
 jmp $ ; jump to current address
@@ -27,8 +35,34 @@ print-bios:
     jmp bios_print
     
 printc:
+    mov ah, 0x0F ; white on black
+    mov cx, ax ; save info in ax
+    movzx ax, byte [ypos]
+    mov dx, 160
+    mul dx
+    movzx bx, byte [xpos]
+    sh1 bx, 1
+    
+    mov di, 0
+    add di, ax ; y offset
+    add di, bx ; x offset
+    
+    mov ax, cx ; restore ax
+    stosw ; write character in al to first byte of video memory, attribute in ah to second
+    add byte [xpos], 1 ; advance x position to the right
+    ret
 
 prints:
+    lodsb
+    test al, al
+    jnz cprint
+    add byte [ypos], 1 ; go down 
+    mov byte [xpos], 0 ; go all the way to left
+    ret
+    
+
+print16:
+
     
 done:
     ret
